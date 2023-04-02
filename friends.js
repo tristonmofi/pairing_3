c0b2c586103ef26a63d5a71c053828c792fd6acc
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-04-01 19:49:11 -0600

    create a new function

--- friends.js
+++ friends.js
@@ -5,3 +5,8 @@ for (var i = 0; i < numbers.length; i++) {
         console.log(numbers[i]);
     }
 }
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] <= 60) {
+        console.log(numbers[i]);
+    }
+}
